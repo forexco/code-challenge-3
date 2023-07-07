fcd1276c7de74c90619e40b4bc027a337d0cdf58
Christopher Menendez <chris96u@192-168-1-100.tpgi.com.au>
2023-07-08 00:42:02 +1000

    feat: add mui library + initial config

---
+++ fx-frontend/src/MuiStyles.d.ts
@@ -0,0 +1,43 @@
+import "@mui/material/styles";
+declare module "@mui/material/styles" {
+  interface Palette {
+    bluePrimary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface PaletteOptions {
+    bluePrimary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface Palette {
+    blueSecondary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface PaletteOptions {
+    blueSecondary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface Palette {
+    blueTertiary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface PaletteOptions {
+    blueTertiary: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface Palette {
+    blueBackground: {
+      main: React.CSSProperties["color"];
+    };
+  }
+  interface PaletteOptions {
+    blueBackground: {
+      main: React.CSSProperties["color"];
+    };
+  }
+}
---
+++ fx-frontend/src/config/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    bluePrimary: {
+      main: "#33428E",
+    },
+    blueSecondary: {
+      main: "#012754",
+    },
+    blueTertiary: {
+      main: "#002854",
+    },
+    blueBackground: {
+      main: "#F8F9FB",
+    },
+  },
+});
+
+export default theme;
