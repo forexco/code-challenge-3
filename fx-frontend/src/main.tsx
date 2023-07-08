407ebc4166b706a905b697ca612e723d1283e8a2
Christopher Menendez <chris96u@192-168-1-100.tpgi.com.au>
2023-07-08 12:24:55 +1000

    feat: setup react-query

--- fx-frontend/src/main.tsx
+++ fx-frontend/src/main.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App.tsx";
 import theme from "./config/theme.ts";
 import { router } from "./routes/routes.tsx";
-import { RouterProvider } from "react-router-dom";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   </React.StrictMode>
 );
